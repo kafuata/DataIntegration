de1b996eec619d45e344d190dd58ef36a94792e0
Clarisse LAWSON <clarisselawson1@gmail.com>
2016-06-22 00:47:08 +0200

    find filename with levenshtein and upload image

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
 "use strict";
 
+let fs = require("fs");
+let path = require("path");
+let levenshtein = require('fast-levenshtein');
+
 let xlsx = require("xlsx");
 let request = require("request-promise");
 
+let imagesDirectory = "/home/clarisse/Downloads/erc/PhotosErc/";
+let serverUploadURL = "http://benjaminbeguin.com/erc/back/traitement_img.php";
 let serverBulkURL = "http://benjaminbeguin.com/erc/back/traitement.php";
 let serverSingleURL = "http://benjaminbeguin.com/erc/back/traitement_id.php";
 let workbook = xlsx.readFile("data.xlsx");
+
+let lang = 'en';
 
 let sheetNames = process.argv.slice(2);
 if (sheetNames.length === 0) {
   sheetNames = workbook.SheetNames; // use all sheets
 }
-
 let rawDataBySheet = sheetNames.map(function (sheetName) {
   return xlsx.utils.sheet_to_json(workbook.Sheets[sheetName]);
 }); 
 let rawData = Array.prototype.concat.apply([], rawDataBySheet);
 
 let json = rawData.map(recordConverter);
-let formData = { json: JSON.stringify(json) };
+let formData = { json: JSON.stringify(json), lang: lang };
+
+let fileNames = fs.readdirSync(imagesDirectory);
+
+/*
+json.forEach(findBestMatch);
+process.exit();
+*/
 
 console.log("Processing " + json.length + " records.");
 
@@ -32,21 +46,47 @@ Promise.all(json.map(sendSingleRequest))
   console.log("All ID requests successfully sent.");
 });
 
+
 function sendSingleRequest(singleJson, id) {
   console.log("Sending ID request for id = " + id + " ...");
 
-  return request.post({ url: serverSingleURL, form: {
-    json: JSON.stringify([{story:"", tags:[], id: id, json: id}]),
-    id: id,
-    timestamp: Date.now()
-  }}); 
+  let match = findBestMatch(singleJson); 
+  let filePath = imagesDirectory + match.fileName;
+
+  if (match.distance > 2) {
+    // match not good enough, skip
+    return Promise.resolve(true);
+  }
+
+  return request
+    .post({ url: serverSingleURL, form: {
+      json: JSON.stringify([{story:"", tags:[], id: id, json: id}]),
+      id: id,
+      lang: lang
+    }})
+    .then(function () {
+      return request.post({
+        url: serverUploadURL,
+        formData: {
+          ID: id,
+          NAME: singleJson.picture,
+          MAX_FILE_SIZE: 500000,
+          image: fs.createReadStream(filePath)
+        }
+      });
+    })
+    .then(function () {
+      process.stdout.write("."); // barre de progression
+    });
 }
 
 function recordConverter(obj, index) {
+  let firstname = obj["Prénom"].trim();
+  let lastname = obj["NOM"].trim();
   return {
-    "picture": "http:\/\/placehold.it\/50x80",
-    "firstname": obj["Prénom"],
-    "lastname": obj["NOM"],
+    "picture": firstname.toLowerCase() + lastname.toLowerCase() + Date.now() + ".jpg",
+    "firstname": firstname,
+    "lastname": lastname,
     "projectname": obj["nom du projet"],
     "gender": obj["Sexe"] || "male",
     "institute": obj["Institut de rattachement"],
@@ -60,4 +100,31 @@ function recordConverter(obj, index) {
     "json" : index,
     "erc_type": obj["Type d\'ERC"]
   }; 
+}
+
+function findBestMatch (item) {
+  var currentName = item.firstname.toLowerCase() + ' ' + item.lastname.toLowerCase();
+
+  let sigles = ['cog', 'stg', 'adg', 'insb', 'inp', 'inee'];
+
+  function clean (name) {
+    name = name.toLowerCase();
+    //filename.toLowerCase().replace(/-/g, ' ').split(' ').slice(0, 2).join(' '),
+   
+      name = name.replace(new RegExp('.jpg'), '');
+    return name;
+  }
+
+  let distances = fileNames.map(filename => levenshtein.get(
+       clean(filename),
+        currentName
+  ));
+
+  let minDistance = Math.min.apply(null, distances);
+  let index = distances.indexOf(minDistance);
+
+  // console.log(minDistance + ': ' + currentName + " => " + fileNames[index]);
+  // if (minDistance <= 2) console.log(fileNames[index]);
+
+  return { distance: minDistance, fileName: fileNames[index] };
 }
